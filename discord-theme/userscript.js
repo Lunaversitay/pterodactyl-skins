ff854892e5a05b27fcbbc175fec990bbd00e1ab8
Luna Versity <18021969+Lunaversitay@users.noreply.github.com>
2018-06-08 21:34:54 -0700

    Create userscript.js
    tamper monkey theme support

---
+++ discord-theme/userscript.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Discord Theme
+// @namespace    https://lunaversity.me
+// @version      0.2
+// @description  Easiest way to use the theme (noticiations are exclusive to gamepanel.co)
+// @author       Luna
+// @match        https://gamepanel.co/*
+// @grant        none
+// ==/UserScript==
+
+
+var head = document.getElementsByTagName('head')[0];
+var body = document.getElementsByTagName('body')[0];
+var link = document.createElement('link');
+link.id = "better_panel";
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = 'https://raw.githubusercontent.com/Lunaversitay/pterodactyl-skins/master/discord-theme/discord.css';
+link.media = 'all';
+head.appendChild(link);
+document.getElementById('notifications-markallread').children[0].style = "";
+document.getElementsByName("query")[0].style.width = "100%"; // drizzy thing he did on gamepanel.co
